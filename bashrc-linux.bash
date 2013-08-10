aa01e92a2776a82238c69102b412952112f5be1d
Daniel Cranston <00ff00@gmail.com>
2013-08-11 01:49:31 +1200

    bashrc-linux: tidy-up 2

--- bashrc-linux.bash
+++ bashrc-linux.bash
@@ -3,17 +3,14 @@
 
 # don't put duplicate lines or lines starting with space in the history. See bash(1) for more options
 HISTCONTROL=ignoreboth
-
 # append to the history file, don't overwrite it
 shopt -s histappend
-
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
 
 # check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
 shopt -s checkwinsize
-
 # If set, the pattern "**" used in a pathname expansion context will match all files and zero or more directories and subdirectories.
 #shopt -s globstar
 
@@ -21,9 +18,7 @@ shopt -s checkwinsize
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
+[ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ] && debian_chroot=$(cat /etc/debian_chroot)
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -47,9 +42,8 @@ ssh-add ~/.ssh/greyrimu_github_rsa &> /dev/null
 ### Tmux ###
 # Set default directory for new windows to home directory
 tmux set-option default-path ~ > /dev/null
-
 # Auto-attach
-if [ -z "$(ps ux | grep "tmux" | grep -v "grep")" ]; then
+if [ -z "$(ps x | grep [t]mux)" ]; then
     echo "[tmux: starting]"
     tmux
 elif [ -z "$TMUX" ]; then
@@ -63,7 +57,7 @@ alias lll='ls -alF | less'
 
 ###Scripts###
 alias mcrun='callDir=$PWD; cd ~/minecraft/server && ./run; cd $callDir; unset callDir'
-alias mckill='kill -9 $(ps x | grep "[m]inecraft" | awk '"'"'{print $1}'"'"')'
+alias mckill='pkill -9 -f minecraft'
 alias tma='tmux attach'
 alias cllog='tail -20 ~/.scripts/cron/checklines.run.log | less'
 
