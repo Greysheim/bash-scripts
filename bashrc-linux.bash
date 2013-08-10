1a5fbab9708ddd2cbea861896c6706e58bc6184b
Daniel Cranston <00ff00@gmail.com>
2013-08-11 01:17:18 +1200

    bashrc-linux: tidy-up and remove redundancies

--- bashrc-linux.bash
+++ bashrc-linux.bash
@@ -1,8 +1,7 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
+# don't put duplicate lines or lines starting with space in the history. See bash(1) for more options
 HISTCONTROL=ignoreboth
 
 # append to the history file, don't overwrite it
@@ -12,12 +11,10 @@ shopt -s histappend
 HISTSIZE=1000
 HISTFILESIZE=2000
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
+# check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
+# If set, the pattern "**" used in a pathname expansion context will match all files and zero or more directories and subdirectories.
 #shopt -s globstar
 
 # make less more friendly for non-text input files, see lesspipe(1)
@@ -28,50 +25,18 @@ if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
-esac
-
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
 fi
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
-# Alias definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
-# See /usr/share/doc/bash-doc/examples in the bash-doc package.
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
-fi
 
 ##### Daniel Cranston's Additions #####
 
@@ -80,7 +45,7 @@ eval $(ssh-agent) &> /dev/null
 ssh-add ~/.ssh/greyrimu_github_rsa &> /dev/null
 
 ### Tmux ###
-# Set default directory for new windows to home directory~
+# Set default directory for new windows to home directory
 tmux set-option default-path ~ > /dev/null
 
 # Auto-attach
@@ -93,17 +58,14 @@ elif [ -z "$TMUX" ]; then
 fi 
 
 ###Aliases###
+alias ll='ls -alF'
 alias lll='ls -alF | less'
 
 ###Scripts###
-alias mcrun="cd ~/minecraft/server && ./run; cd"
-alias mckill='kill -9 $(ps x | grep minecraft | grep -v grep | awk '"'"'{print $1}'"'"')'
-alias tma="tmux attach"
-alias cllog="tail -20 ~/.scripts/cron/checklines.run.log | less"
-
-#### MotD ####
-#echo "`uname -sr`, `uname -m`; `uname -n`"
-#echo "Successful login. Welcome, $USER. (^_^);;"
+alias mcrun='callDir=$PWD; cd ~/minecraft/server && ./run; cd $callDir; unset callDir'
+alias mckill='kill -9 $(ps x | grep "[m]inecraft" | awk '"'"'{print $1}'"'"')'
+alias tma='tmux attach'
+alias cllog='tail -20 ~/.scripts/cron/checklines.run.log | less'
 
 ### Functions ###
 
